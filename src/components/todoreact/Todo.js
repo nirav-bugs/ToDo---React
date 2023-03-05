3562d6bececf423028aaedcb7945586017f40f63
tech-nirav <21it1@svitvasad.ac.in>
2023-03-05 12:39:36 +0530

    Done edit functionality

--- src/components/todoreact/Todo.js
+++ src/components/todoreact/Todo.js
@@ -1,11 +1,11 @@
-import React, { useState , useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import "./style.css"
-const getLocaldata=()=>{
-    const lists=localStorage.getItem("mytodolist")
-    if(lists){
+const getLocaldata = () => {
+    const lists = localStorage.getItem("mytodolist")
+    if (lists) {
         return JSON.parse(lists);
     }
-    else{
+    else {
         return [];
     }
 }
@@ -13,64 +13,65 @@ const Todo = () => {
     const [inputdata, setinputdata] = useState("")
     const [items, setItems] = useState(getLocaldata())
     const [isedit, setisedit] = useState("")
-    const [tooglebtn,settooglebtn ] = useState(false)
+    const [tooglebtn, settooglebtn] = useState(false)
 
     // add items fun 
-    const addItem =async () => {
+    const addItem = async () => {
         if (!inputdata) {
             alert("Plz enter data")
         }
-        else if(inputdata && tooglebtn) {
-                setItems(
-                    items.map((curElem)=>{
-                        if(curElem.id===isedit){
-                            return {...curElem,name:inputdata}
-                        }
-                        return curElem
-                    })
-                )
-                setinputdata("") 
-                setisedit(null)
-                settooglebtn(false)
+        else if (inputdata && tooglebtn) {
+            setItems(
+                items.map((curElem) => {
+                    if (curElem.id === isedit) {
+                        return { ...curElem, name: inputdata }
+                    }
+                    return curElem
+                })
+            )
+            setinputdata("")
+            setisedit(null)
+            settooglebtn(false)
         }
         else {
-            const mynewdata=({
-                id:new Date().getTime().toString(),
-                name:inputdata}
-            ); 
-            setItems([ ...items, mynewdata ])
+            const mynewdata = ({
+                id: new Date().getTime().toString(),
+                name: inputdata
+            }
+            );
+            setItems([...items, mynewdata])
             setinputdata("");
         }
 
     }
     // edit section  
-const editItem=(index)=>{
-     const item_todo_edit=items.find((curElem)=>{
-            return curElem.id===index
-     })
-     setinputdata(item_todo_edit.name) 
-     setisedit(index)
-     settooglebtn(true)
+    const editItem = (index) => {
+        const item_todo_edit = items.find((curElem) => {
+            return curElem.id === index
+        })
+        setinputdata(item_todo_edit.name)
+        setisedit(index)
+        settooglebtn(true)
 
-}
+    }
 
 
     // delete section
-     const deleteItem=(index)=>{
-            const updatedItem=items.filter((curElem)=>{
-                return curElem.id!==index
+    const deleteItem = (index) => {
+        const updatedItem = items.filter((curElem) => {
+            return curElem.id !== index
         })
         setItems(updatedItem)
-     }
+    }
     //  remove all
-     const deleteall=()=>{
-         setItems([])
-     }
+    const deleteall = () => {
+        setItems([])
+    }
 
     //  localstorage
-    useEffect(()=>{
-        localStorage.setItem("mytodolist",JSON.stringify(items))
-    },[items])
+    useEffect(() => {
+        localStorage.setItem("mytodolist", JSON.stringify(items))
+    }, [items])
 
 
     return (
@@ -82,9 +83,9 @@ const editItem=(index)=>{
                         <figcaption>Add your list here</figcaption>
                     </figure>
                     <div className="addItems">
-                        <input type="text" placeholder='Enter your text' className='form-control' value={inputdata} onChange={(event) => setinputdata(event.target.value)    }/>
-                    {tooglebtn ?  <i className="far add-btn fa-edit " onClick={addItem}></i>:  <i className="fa fa-plus add-btn " onClick={addItem}></i>}
-                       
+                        <input type="text" placeholder='Enter your text' className='form-control' value={inputdata} onChange={(event) => setinputdata(event.target.value)} />
+                        {tooglebtn ? <i className="far add-btn fa-edit " onClick={addItem}></i> : <i className="fa fa-plus add-btn " onClick={addItem}></i>}
+
 
                     </div>
 
@@ -95,8 +96,8 @@ const editItem=(index)=>{
                                 <div className="eachItem" key={curElem.id}>
                                     <h3>{curElem.name}</h3>
                                     <div className="toto-btn" >
-                                        <i className="far add-btn fa-edit mx-3" onClick={()=>{editItem(curElem.id)}}></i>
-                                        <i className="far fa-trash-alt add-btn" onClick={( )=>{
+                                        <i className="far add-btn fa-edit mx-3" onClick={() => { editItem(curElem.id) }}></i>
+                                        <i className="far fa-trash-alt add-btn" onClick={() => {
                                             deleteItem(curElem.id)
                                         }}> </i>
                                     </div>
