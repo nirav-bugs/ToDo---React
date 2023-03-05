17ba12099c53baddeccfc8af46109437c799161e
tech-nirav <21it1@svitvasad.ac.in>
2023-03-05 10:48:27 +0530

    Add and remove one or all functionality.

--- src/components/todoreact/Todo.js
+++ src/components/todoreact/Todo.js
@@ -5,16 +5,16 @@ const Todo = () => {
     const [items, setItems] = useState([])
 
     // add items fun
-    const addItem = () => {
+    const addItem =async () => {
         if (!inputdata) {
             alert("Plz enter data")
         }
         else {
-            const mynewdata=([{
+            const mynewdata=({
                 id:new Date().getTime().toString(),
-                name:inputdata,
-            }]);
-            setItems([...items, mynewdata])
+                name:inputdata}
+            ); 
+            setItems([ ...items, mynewdata ])
             setinputdata("");
         }
 
@@ -26,6 +26,11 @@ const Todo = () => {
         })
         setItems(updatedItem)
      }
+    //  remove all
+     const deleteall=()=>{
+         setItems([])
+     }
+
 
     return (
         <>
@@ -36,8 +41,7 @@ const Todo = () => {
                         <figcaption>Add your list here</figcaption>
                     </figure>
                     <div className="addItems">
-                        <input type="text" placeholder='Enter your text' className='form-control' value={inputdata} onChange={(event) => setinputdata(event.target.value) &&
-                            console.log(event.target.value)} />
+                        <input type="text" placeholder='Enter your text' className='form-control' value={inputdata} onChange={(event) => setinputdata(event.target.value)    }/>
 
                         <i className="fa fa-plus add-btn " onClick={addItem}></i>
 
@@ -48,7 +52,7 @@ const Todo = () => {
                         {items.map((curElem) => {
                             return (
                                 <div className="eachItem" key={curElem.id}>
-                                    <h3>{curElem}</h3>
+                                    <h3>{curElem.name}</h3>
                                     <div className="toto-btn" >
                                         <i className="far add-btn fa-edit mx-3"></i>
                                         <i className="far fa-trash-alt add-btn" onClick={( )=>{
@@ -65,7 +69,7 @@ const Todo = () => {
 
                     {/* remove all btn */}
                     <div className="showItems">
-                        <button className='btn effect04' data-sm-link-text="Remove All"><span> Check list</span></button>
+                        <button className='btn effect04' data-sm-link-text="Remove All" onClick={deleteall}><span> Check list</span></button>
                     </div>
                 </div>
             </div>
